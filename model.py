b7b23efeecac8a4420e302ca6b2656bf56d60143
Ace-Miky <153847576+Ace-Miky@users.noreply.github.com>
2024-02-11 12:06:29 +0100

    Create model.py

---
+++ model.py
@@ -0,0 +1,30 @@
+def is_regular_matrix(matrix):
+    for row in matrix:
+        if len(row) != len(matrix[0]):
+            return False
+    return True
+
+
+def set_matrix_element(matrix, row, column, value):
+    assert row < len(matrix)
+    assert column < len(matrix[0])
+    assert value == 0 or value == 1
+    matrix[row][column] = value
+
+
+# ritorna l'elemento row, column della matrix
+def get_matrix_element(matrix, row, column):
+    assert row < len(matrix)
+    assert column < len(matrix[0])
+    return matrix[row][column]
+
+
+def print_matrix(matrix):
+    for row in matrix:
+        row_string = ""
+        for element in row:
+            if element == 0:
+                row_string = row_string + "O"
+            else:
+                row_string = row_string + "X"
+        print(row_string)
