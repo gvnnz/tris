b21a87ed8144f6442287e9ea21f3a0fc874028f1
Ace-Miky <153847576+Ace-Miky@users.noreply.github.com>
2024-03-15 15:47:17 +0100

    Aggiunto cambio giocatori (#23)

--- main.py
+++ main.py
@@ -5,17 +5,15 @@ matrix = model.Matrix(3, 3)
 
 
 def is_valid_input(input_string, matrix):
-    if len(input_string) != 3:
+    if len(input_string) != 2:
         return False
-    if input_string[0] != "o" and input_string[0] != "x":
-        return False
-    if input_string[1].isdigit() == False or input_string[2].isdigit() == False:
+    if input_string[0].isdigit() == False or input_string[1].isdigit() == False:
         return False
     if (
-        int(input_string[1]) >= 0
-        and int(input_string[1]) < matrix.count_rows()
-        and int(input_string[2]) >= 0
-        and int(input_string[2]) < matrix.count_columns()
+        int(input_string[0]) >= 0
+        and int(input_string[0]) < matrix.count_rows()
+        and int(input_string[1]) >= 0
+        and int(input_string[1]) < matrix.count_columns()
     ):
         return True
     return False
@@ -23,11 +21,10 @@ def is_valid_input(input_string, matrix):
 
 def parse_input(input_string, matrix):
     assert is_valid_input(input_string, matrix)
-    symbol_value = 2 if input_string[0] == "x" else 1
-    row_value = int(input_string[1])
-    column_value = int(input_string[2])
+    row_value = int(input_string[0])
+    column_value = int(input_string[1])
 
-    return (symbol_value, row_value, column_value)
+    return (row_value, column_value)
 
 
 def sanitize_input(input_string):
@@ -35,13 +32,19 @@ def sanitize_input(input_string):
 
 
 # -----------------------------------------------------------------------------------------------
+count = 0
 while True:
-    input_string = sanitize_input(input("Digita o oppure x seguita dalle coordinate: "))
+    if count % 2 == 0:
+        symbol_value = 1
+    else:
+        symbol_value = 2
+    count = count + 1
+    input_string = sanitize_input(input("Insert the coordinates: "))
     if is_valid_input(input_string, matrix) == False:
         print("Invalid input!!!")
         continue
     input_tuple = parse_input(input_string, matrix)
-    matrix.set_element(input_tuple[1], input_tuple[2], input_tuple[0])
+    matrix.set_element(input_tuple[0], input_tuple[1], symbol_value)
     view.print_matrix(matrix)
     if model.is_winner(matrix, 1):
         print("Player 1 WIN!!!")
